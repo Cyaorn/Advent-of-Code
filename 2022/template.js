55514decdd25590cc888098e79c2576c360e12ab
Ciaran Diep <diepc@uchicago.edu>
2023-11-07 15:02:29 -0500

    edited template a bit

--- 2022/template.js
+++ 2022/template.js
@@ -1,7 +1,7 @@
 const fs = require('fs')
 
 function main() {
-    let filedata = fs.readFileSync('01.txt').toString().split('\r\n\r\n');
+    let filedata = fs.readFileSync('.txt').toString().split('\r\n\r\n');
 }
 
 main();
