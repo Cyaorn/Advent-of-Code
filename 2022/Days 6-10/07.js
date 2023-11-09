b3087c4f4c13d7b18e986ef85275940eca338c7b
Ciaran Diep <diepc@uchicago.edu>
2023-11-09 16:30:17 -0500

    Day 7 completed, tfw recursion is hard

--- 2022/Days 6-10/07.js
+++ 2022/Days 6-10/07.js
@@ -76,6 +76,75 @@ function main() {
             i++; // increment only in this clause because ls command autoincrement already
         }
     }
+    // root.printTree();
+    const drive_space = 70000000;
+    const update_size = 30000000;
+    const space_needed = update_size - (drive_space - root.getFileSize());
+    console.log(space_needed);
+    let directories = new Array();
+    compile(root, directories);
+    directories.sort((a, b) => a[1] - b[1]);
+    console.log(directories.filter((dir) => dir[1] >= space_needed)[0]);
+}
+
+// I gave up trying to do a recursive solution because it's easier to just fill an array with
+// all of the directories and their sizes
+function compile(tree, array) { // still inefficient in the same way
+    if (tree.filetype === filetype.File) {
+        return;
+    }
+    tree.children.filter((child) => child.filetype === filetype.Directory)
+      .forEach((child) => {
+            compile(child, array);
+            array.push([child.filename, child.getFileSize()]);
+        }
+    )
+    array.push([tree.filename, tree.getFileSize()]);
+}
+
+/* Part 1, uses the same FileTree object
+function main() {
+    let filedata = fs.readFileSync('07.txt').toString().split('\r\n');
+    let root = new FileTree(filetype.Directory, 0, '/', null);
+    let current = root;
+    let i = 1; // first line is the only '$ cd /' command
+
+    // build FileTree
+    while (i < filedata.length) {
+        // console.log(`Line ${i}: ` + filedata[i]);
+        if (filedata[i].substring(0, 4) === '$ ls') { // establish children
+            // console.log(`ls command detected, showing contents of ${current.filename}`);
+            i++;
+            while (i < filedata.length && filedata[i].substring(0, 2) !== '$ ') { // while there are no commands next, build tree structure
+                // console.log(filedata[i]);
+                if (filedata[i].substring(0, 4) === 'dir ') { // child directory
+                    current.children.push(
+                      new FileTree(filetype.Directory, 0, filedata[i].substring(4), current)
+                    );
+                } else { // split by space, first part is size, second part is name
+                    token = filedata[i].split(' ');
+                    current.children.push(
+                      new FileTree(filetype.File, parseInt(token[0]), token[1], current)
+                    );
+                }   
+                i++;
+            }
+            // console.log(current.children);
+        } else if (filedata[i].substring(0, 5) === '$ cd ') {
+            if (filedata[i].substring(5) === '..') {
+                // console.log(`Changing directory to ${current.parent.filename}`);
+                current = current.parent;
+            } else {
+                // console.log(`Changing directory to ${filedata[i].substring(5)}`);
+                current.children.forEach((child) => { // a bit inefficient, would probably be better with hashing
+                    if (child.filename === filedata[i].substring(5)) {
+                        current = child;
+                    }
+                });
+            }
+            i++; // increment only in this clause because ls command autoincrement already
+        }
+    }
     root.printTree();
 
     console.log(traverse(root));
@@ -97,6 +166,6 @@ function traverse(tree) { // This is very inefficient because it runs getFileSiz
         out += total_size;
     }
     return out;
-}
+} */
 
 main();
