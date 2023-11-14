577fdc4495d71ae851d0549dd648a499c08fb27a
Ciaran Diep <diepc@uchicago.edu>
2023-11-14 15:45:47 -0500

    Day 11 Part 2 completed, instructions were a bit confusing, may have cheated a bit to figure out what to do

--- 2022/Days 11-15/11.js
+++ 2022/Days 11-15/11.js
@@ -22,6 +22,72 @@ function generateFunction(tokens) { // function to generate a new function that 
     }
 }
 
+function gcd(a, b) { 
+    for (let temp = b; b !== 0;) { 
+        b = a % b; 
+        a = temp; 
+        temp = b; 
+    } 
+    return a; 
+} 
+  
+function get_lcm(a, b) { 
+    return (a * b) / gcd(a, b); 
+} 
+
+
+function main() {
+    let filedata = fs.readFileSync('11.txt').toString().split('\r\n\r\n');
+    let monkeys = [];
+    // biggest issue with this problem will be handling the input (since I would rather not just hard-code the parameters)
+    filedata.forEach((monkey) => {
+        lines = monkey.split('\r\n');
+        // retrieve starting objects
+        objects = lines[1].substring('  Starting items: '.length).split(', ');
+        objects = objects.map((o) => parseInt(o));
+
+        // retrieve operation
+        operation_tokens = lines[2].substring('  Operation: new = '.length).split(' ');
+        operation = generateFunction(operation_tokens);
+
+        // retrieve divisor and true/false cases
+        divisor = parseInt(lines[3].substring('  Test: divisible by '.length));
+        true_case = parseInt(lines[4].substring('    If true: throw to monkey '.length));
+        false_case = parseInt(lines[5].substring('    If false: throw to monkey '.length));
+        
+        monkeys.push(new Monkey(objects, operation, divisor, true_case, false_case));
+    });
+    const lcm = monkeys.map(m => m.divisor).reduce((acc, d) => get_lcm(acc, d), 1);
+    current_worry = 0;
+    for (round = 0; round < 10000; round++) {
+        for (i = 0; i < monkeys.length; i++) {
+            // console.log(`\nMonkey ${i}`);
+            while(monkeys[i].objects.length > 0) {
+                current_obj = monkeys[i].objects.shift();
+                // console.log(`Current object: ${current_obj}`);
+                current_worry = monkeys[i].operation(current_obj);
+                // got confused about this but basically instead of dividing by 3 you divide by the LCM of all the monkey divisors
+                current_worry = current_worry % lcm;
+                if (current_worry % monkeys[i].divisor === 0) {
+                    // console.log(`Throwing ${current_worry} to Monkey ${monkeys[i].true_case}`);
+                    monkeys[monkeys[i].true_case].objects.push(current_worry);
+                } else {
+                    // console.log(`Throwing ${current_worry} to Monkey ${monkeys[i].false_case}`);
+                    monkeys[monkeys[i].false_case].objects.push(current_worry);
+                }
+                monkeys[i].activity++;
+            }
+            if (round === 0 || round === 19) {
+                // console.log(`Monkey ${i} Activity: ${monkeys[i].activity}`);
+            }
+        }
+        // console.log(monkeys.map(m => m.objects));
+    }
+    monkeys.sort((a, b) => b.activity - a.activity);
+    console.log(monkeys[0].activity * monkeys[1].activity);
+}
+
+/* Part 1
 function main() {
     let filedata = fs.readFileSync('11.txt').toString().split('\r\n\r\n');
     let monkeys = [];
@@ -67,6 +133,6 @@ function main() {
     }
     monkeys.sort((a, b) => b.activity - a.activity);
     console.log(monkeys[0].activity * monkeys[1].activity);
-}
+} */
 
 main();
