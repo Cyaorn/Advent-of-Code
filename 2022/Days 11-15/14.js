135e869010827d24818b4942109fd2cd2517b744
Cyaorn <diepc153@gmail.com>
2023-12-01 15:55:40 -0500

    Day 14 Part 1 completed, pretty straightforward

---
+++ 2022/Days 11-15/14.js
@@ -0,0 +1,71 @@
+const fs = require('fs')
+
+function drop_sand(cave) { // return true if sand sticks, false if sand falls off
+    const x_drop = 500;
+    const y_drop = 0;
+    let x = x_drop;
+    let y = y_drop;
+    while (y < cave.length - 1) {
+        if (cave[y + 1][x] === '.') { // can go straight down
+            y++;
+        } else if (cave[y + 1][x - 1] === '.') { // can go down-left
+            y++;
+            x--;
+        } else if (cave[y + 1][x + 1] === '.') { // can go down-right
+            y++;
+            x++;
+        } else {
+            cave[y][x] = 'o';
+            return true;
+        }
+    }
+    return false;
+}
+
+function main() {
+    let filedata = fs.readFileSync('14.txt').toString().split('\r\n');
+    // approximations of bounds of cave
+    const height = 200;
+    const width = 600;
+    let cave = Array(height).fill(0).map(_ => (new Array(width)).fill('.'));
+    // print_cave(cave);
+    filedata.forEach((line) => {
+        segments = line.split(' -> ').map(
+          (coords) => coords.split(',').map((value) => parseInt(value)));
+        segments.forEach((segment, i) => {
+            if (i < segments.length - 1) {
+                x0 = segment[0];
+                x1 = segments[i + 1][0];
+                y0 = segment[1];
+                y1 = segments[i + 1][1];
+                // console.log(x0, y0, x1, y1);
+                if (y0 != y1) { // differ by y-value
+                    for (j = Math.min(y0, y1); j <= Math.max(y0, y1); j++) {
+                        cave[j][x0] = '#';
+                    }
+                } else { // differ by x-value
+                    for (j = Math.min(x0, x1); j <= Math.max(x0, x1); j++) {
+                        cave[y0][j] = '#';
+                    }
+                }
+            } 
+        });
+    });
+    let sand_count = 0;
+    while (drop_sand(cave)) {
+        sand_count++;
+    }
+    console.log(sand_count);
+}
+
+function print_cave(cave) {
+    for (i = 0; i < cave.length; i++) {
+        out = '';
+        for (j = 450; j < cave[i].length - 50; j++) {
+            out += cave[i][j];
+        }
+        console.log(out, out.length);
+    }
+}
+
+main();
