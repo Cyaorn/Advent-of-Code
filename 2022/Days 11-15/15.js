f1bf7e96fe489ee195737a0c5aff76131d5b5594
Cyaorn <diepc153@gmail.com>
2023-12-06 14:11:22 -0600

    Day 15 Part 2 solved, answer takes a few seconds to output but still within reason B)

--- 2022/Days 11-15/15.js
+++ 2022/Days 11-15/15.js
@@ -1,5 +1,72 @@
 const fs = require('fs')
 
+// IDEA FOR PART 2:
+// Just do the same algorithm, but for every target_x and target_y between 0 and 4 mil,
+// find whichever x, y values are such that there are 2 intervals (unless the space is on the edge)
+
+function merge_intervals(intervals) { // assumes array of interval pairs sorted by first element in asc order
+    let out_intervals = new Array();
+    current_min = intervals[0][0];
+    current_max = intervals[0][1];
+    n = 1;
+    while (n < intervals.length) {
+        // console.log(n, ': ', current_min, current_max);
+        if (current_max + 1 < intervals[n][0]) { // if endpoint is out of range of next start point, continue
+            // console.log("Pushing current interval");
+            out_intervals.push([current_min, current_max]);
+            current_min = intervals[n][0];
+            current_max = intervals[n][1];
+        } else {
+            current_max = Math.max(current_max, intervals[n][1]);
+            // console.log("Expanded interval to", current_min, current_max);
+        }
+        n++;
+    }
+    out_intervals.push([current_min, current_max]);
+    return out_intervals;
+}
+
+function main() {
+    let filedata = fs.readFileSync('15.txt').toString().split('\r\n');
+    const y_bound = 4000000;
+    let intervals = new Array(); // store intervals of values because keeping track of the array explicitly is too big
+    let sxs = new Set(); // set of sensors
+    filedata.forEach((line) => {
+        coords = line.substring('Sensor at '.length)
+          .split(': closest beacon is at ')
+          .map((pair) => 
+            pair.substring('x='.length)
+              .split(', y=')
+              .map(num => parseInt(num)));
+        // console.log(coords);
+        sx = coords[0][0];
+        sy = coords[0][1];
+        bx = coords[1][0];
+        by = coords[1][1];
+        sxs.add({'sx': sx, 'sy': sy, 'range': Math.abs(bx - sx) + Math.abs(by - sy)});
+    });
+    // console.log(sxs);
+    for (y = 0; y <= y_bound; y++) { // for every y-level check if there is a break in the interval
+        intervals = new Array();
+        sxs.forEach((sensor) => {
+            extra = sensor.range - Math.abs(sensor.sy - y);
+            // console.log(extra);
+            if (extra >= 0) {
+                intervals.push([Math.max(sensor.sx - extra, 0), Math.min(sensor.sx + extra, y_bound)]);
+            }
+        });
+        if (intervals.length > 0) { 
+            intervals = merge_intervals(intervals.sort((a, b) => a[0] - b[0]));
+            if (intervals.length > 1 || intervals[0][0] > 0 || intervals[0][1] < 4000000) {
+                let target_x = intervals[0][1] + 1;
+                console.log(target_x * 4000000 + y);
+                break;
+            }
+        }
+    }
+}
+
+/* Part 1
 function main() {
     let filedata = fs.readFileSync('15.txt').toString().split('\r\n');
     const target_y = 2000000;
@@ -39,28 +106,6 @@ function main() {
         });
     });
     console.log(count);
-}
-
-function merge_intervals(intervals) { // assumes array of interval pairs sorted by first element in asc order
-    let out_intervals = new Array();
-    current_min = intervals[0][0];
-    current_max = intervals[0][1];
-    n = 1;
-    while (n < intervals.length) {
-        // console.log(n, ': ', current_min, current_max);
-        if (current_max + 1 < intervals[n][0]) { // if endpoint is out of range of next start point, continue
-            // console.log("Pushing current interval");
-            out_intervals.push([current_min, current_max]);
-            current_min = intervals[n][0];
-            current_max = intervals[n][1];
-        } else {
-            current_max = Math.max(current_max, intervals[n][1]);
-            // console.log("Expanded interval to", current_min, current_max);
-        }
-        n++;
-    }
-    out_intervals.push([current_min, current_max]);
-    return out_intervals;
-}
+} */
 
 main();
